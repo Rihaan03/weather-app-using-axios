09c2fbc0a4ad8a81904fd8c4268c5b1c715a00cb
Rihaan03 <avikacharyya868@gmail.com>
2022-07-27 13:22:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import axios from'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ShowData from './ShowData'
+function App(){
+    const[city,setCity]=useState("")
+    const[data,setData]=useState({
+        description:"",
+        temp:0,
+        temp_min:0,
+        temp_max:0,
+        humidity:0,
+        sunrise:0,
+        sunset:0,
+        country:0,
+    })
+    const handleClick=()=>{
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c984bcb51310b48227ac2b00ef1c4d20`).then((response)=>{
+           setData({
+            description:response.data.weather[0].description,
+            temp:response.data.main.temp,
+            temp_min:response.data.main.temp_min,
+            temp_max:response.data.main.temp_max,
+            humidity:response.data.main.humidity,
+            sunrise:response.data.sys.sunrise,
+            sunset:response.data.sys.sunset,
+            country:response.data.sys.country,
+           })
+        })
+    }
+    return(
+      <>
+      <div className='container text-center my-2'>
+          <h2 style={{color:"brown"}}>Weather App Using React JS(Axios)</h2>
+          <input placeholder="enter the place..." type="text" className='from-control' value={city} onChange={(e) => {
+              setCity(e.target.value);
+          }} />
+          <button className='btn btn-danger mx-2' type='submit' onClick={handleClick}>Search weather</button>
+
+      </div>
+
+      <ShowData text ={data}></ShowData>
+  </>
+    )
 }
-
 export default App;
---
+++ src/ShowData.js
@@ -0,0 +1,82 @@
+import React from 'react'
+function ShowData(props){
+    return(
+<div class="container my-5">
+            <div class="row mb-2">
+                <div class="col-md-4">
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Description</h5>
+                            <p class="card-text">{props.text.description}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Temp</h5>
+                            <p class="card-text">{props.text.temp}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Temp Min</h5>
+                            <p class="card-text">{props.text.temp_min}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-md-4">
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Temp Max</h5>
+                            <p class="card-text">{props.text.temp_max}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Humidity</h5>
+                            <p class="card-text">{props.text.humidity}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Sunrise</h5>
+                            <p class="card-text">{props.text.sunrise}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-2">
+                <div class="col-md-4">
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Sunset</h5>
+                            <p class="card-text">{props.text.sunset}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Country</h5>
+                            <p class="card-text">{props.text.country}</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+        </div>
+
+    )
+}
+export default ShowData;
